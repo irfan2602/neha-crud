385fa08bde043e2105bc915ed20c5e4e4b756e7b
Irfan Ahmad <meirfan26@gmail.com>
2023-08-23 21:53:37 +0530

    commit

--- src/dist/dto/create-user.dto.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class CreateUserDto {
-    readonly firstName: string;
-    readonly lastName: number;
-    readonly email: string;
-    readonly age: number;
-}
--- src/dist/interface/user.interface.d.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from 'mongoose';
-export interface IUser extends Document {
-    readonly firstName: string;
-    readonly lastName: number;
-    readonly email: string;
-    readonly age: number;
-}
